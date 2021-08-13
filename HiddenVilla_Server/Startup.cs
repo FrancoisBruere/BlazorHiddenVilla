3232f8ac6b8d0bf8c91d83bb5071b2a13760f5e9
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-08-13 19:27:21 +0200

    Implement AutoMapper Implement Repository CreateHotelRooms

--- HiddenVilla_Server/Startup.cs
+++ HiddenVilla_Server/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace HiddenVilla_Server
 {
@@ -30,6 +31,7 @@ namespace HiddenVilla_Server
         {
             services.AddDbContext<ApplicationDbContext>(options=>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
