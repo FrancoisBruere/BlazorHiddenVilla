a5632bfe98fa6ab868acedb9ec0af5d0643443bb
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-09-02 08:25:27 +0200

    Create sing in DTO's

---
+++ Models/AuthenticationResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AuthenticationResponseDTO
+    {
+        public bool isAuthSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        public string Token { get; set; }
+        public UserDTO UserDTO { get; set; }
+    }
+}
