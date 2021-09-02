4207ab1f7a4aa8937d02371bd2f19dfa24dbfeff
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-09-02 07:25:32 +0200

    Add Singup endpoint - registation user

--- HiddenVilla_Api/Controllers/AccountController.cs
+++ HiddenVilla_Api/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using DataAccess.Data;
+﻿using Common;
+using DataAccess.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,44 @@ namespace HiddenVilla_Api.Controllers
             _userManager = userManager;
             _roleManager = roleManager;
         }
-        
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> SignUp([FromBody] UserRequestDTO userRequestDTO)
+        {
+            if (userRequestDTO == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = userRequestDTO.Email,
+                Email = userRequestDTO.Email,
+                Name = userRequestDTO.Name,
+                PhoneNumber = userRequestDTO.PhoneNo,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user,userRequestDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new RegistrationResponceDTO { Errors = errors, isRegistrationSuccessful = false });
+            }
+
+            var roleResult = await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+
+            if (!roleResult.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new RegistrationResponceDTO { Errors = errors, isRegistrationSuccessful = false });
+            }
+
+            return StatusCode(201);
+        }
+
+
     }
 }
