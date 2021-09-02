50b20012b3c053190aeb72eca5a8c81fe10e3324
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-09-02 11:51:47 +0200

    Add configure startup.cs For API Settings Key AddAuthentication and JwtBearer

--- HiddenVilla_Api/Startup.cs
+++ HiddenVilla_Api/Startup.cs
@@ -2,6 +2,7 @@ using Business.Repository;
 using Business.Repository.IRepository;
 using DataAccess.Data;
 using HiddenVilla_Api.Helper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,10 +13,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HiddenVilla_Api
@@ -41,7 +44,30 @@ namespace HiddenVilla_Api
             var appSettingSection = Configuration.GetSection("APISettings");
             services.Configure<APISettings>(appSettingSection);
 
+            var apiSettings = appSettingSection.Get<APISettings>(); // pupulate key
+            var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
 
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidAudience = apiSettings.ValidAudience,
+                    ValidIssuer = apiSettings.ValidIssuer,
+                    ClockSkew = TimeSpan.Zero
+
+                };
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
