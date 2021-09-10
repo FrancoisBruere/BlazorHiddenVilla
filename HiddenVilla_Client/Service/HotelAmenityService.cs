eae4bf02324b62ec44d432b293ea20785799e3df
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-09-10 10:25:48 +0200

    Implement Hotel Amenity Service

---
+++ HiddenVilla_Client/Service/HotelAmenityService.cs
@@ -0,0 +1,29 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service
+{
+    public class HotelAmenityService : IHotelAmenityService
+    {
+        private readonly HttpClient _client;
+        public HotelAmenityService(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<IEnumerable<HotelAmenityDTO>> GetHotelAmenities()
+        {
+
+            var responce = await _client.GetAsync($"api/HotelAmenity/");
+            var content = await responce.Content.ReadAsStringAsync();
+            var amenities = JsonConvert.DeserializeObject<IEnumerable<HotelAmenityDTO>>(content);
+            return amenities;
+
+        }
+    }
+}
