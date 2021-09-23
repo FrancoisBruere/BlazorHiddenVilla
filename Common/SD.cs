13b24bd16e9975308a8a7d623cd28bc5c2c18192
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-09-23 10:48:10 +0200

    Add jwtParser and inplement AuthStateProvider with getauthenticationstate

--- Common/SD.cs
+++ Common/SD.cs
@@ -14,6 +14,7 @@ namespace Common
 
         public const string Local_InitialBooking = "InitialRoomBookingInfo";
         public const string Local_RoomOrderDetails = "RoomOrderDetails";
+        public const string Local_Token = "JWT Token";
 
         public const string Status_Pending = "Pending";
         public const string Status_Booked = "Booked";
@@ -21,5 +22,7 @@ namespace Common
         public const string Status_CheckedOut_Completed = "CheckedOut";
         public const string Status_noShow = "NoShow";
         public const string Status_Cancelled = "Canselled";
+
+       
     }
 }
---
+++ HiddenVilla_Client/Service/AuthStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Blazored.LocalStorage;
+using Common;
+using HiddenVilla_Client.Helper;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Service
+{
+    public class AuthStateProvider:AuthenticationStateProvider
+    {
+
+        private readonly HttpClient _httpclient;
+        private readonly ILocalStorageService _localStorage;
+        public AuthStateProvider(HttpClient client, ILocalStorageService localStorage)
+        {
+            _httpclient = client;
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>(SD.Local_Token); // retrive token from local storage
+            if (token == null) // pass anonom request
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            _httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token); //add bearer token to http client
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+        }
+    }
+}
