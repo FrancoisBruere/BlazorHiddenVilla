a4d3462185aafee03f9c7af244d86e0e990a9f4f
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-09-27 08:10:36 +0200

    Add codebind file for register user

---
+++ HiddenVilla_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,55 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+
+        private UserRequestDTO UserForRegistration = new UserRequestDTO();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErros { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+        [Inject]
+        NavigationManager navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+
+            ShowRegistrationErros = false;
+            IsProcessing = true;
+
+            var result = await authenticationService.RegisterUser(UserForRegistration);
+
+
+
+            if (result.isRegistrationSuccessful)
+            {
+                IsProcessing = false;
+                navigationManager.NavigateTo("/login");
+
+
+            }
+            else
+            {
+                IsProcessing = false;
+                Errors = result.Errors;
+                ShowRegistrationErros = true;
+
+            }
+
+        }
+
+
+
+    }
+}
