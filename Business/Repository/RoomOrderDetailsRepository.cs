d3deb202ff360108b23247f9b9b9e383cd154799
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-10-06 08:27:01 +0200

    ImplementroomorderStatus method

--- Business/Repository/RoomOrderDetailsRepository.cs
+++ Business/Repository/RoomOrderDetailsRepository.cs
@@ -109,9 +109,37 @@ namespace Business.Repository
             return new RoomOrderDetailsDTO();
         }
 
-        public Task<bool> UpdateRoomOrderStatus(int roomOrderId, string status)
+        public async Task<bool> UpdateRoomOrderStatus(int roomOrderId, string status)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                var roomOrder = await _db.RoomOrderDetails.FirstOrDefaultAsync(u => u.Id == roomOrderId);
+
+                if (roomOrder == null)
+                {
+                    return false;
+
+                }
+
+                roomOrder.Status = status;
+                if (status ==SD.Status_CheckedIn)
+                {
+                    roomOrder.ActualCheckInDate = DateTime.Now;
+                }
+                if (status == SD.Status_CheckedOut_Completed)
+                {
+                    roomOrder.ActualCheckOutDate = DateTime.Now;
+                }
+                await _db.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+
+            }
         }
     }
 }
